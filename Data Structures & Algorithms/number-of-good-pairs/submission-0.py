6c3263c147037a8075a855932c1858554b1bdcde
Sam Lui <136400772+samlui1009@users.noreply.github.com>
2026-09-24 23:25:32 -0700

    Add: number-of-good-pairs - submission-0

---
+++ Data Structures & Algorithms/number-of-good-pairs/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        res = 0
+        for i in range(0, len(nums), 1):
+            for j in range(i+1, len(nums), 1):
+                if nums[i] == nums[j]:
+                    res += 1
+        return res
+        
